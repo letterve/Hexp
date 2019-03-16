48f5d139e5a794fed728b3a483612d258f642411
letterve <sedvic@gmail.com>
2019-03-16 02:40:33 +0100

    begin

---
+++ Install.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+if os.path.exists('/home/pi/.bashrc') == False
+    print("Bashrc cannot be found")
+    
---
+++ wifiscript.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import os
+
+CurrentDir=os.getcwd()
+
+
+os.path.exists('C:\\Windows')
